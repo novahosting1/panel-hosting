f9fbbdb3f779e330dfe56fb13405f3210b96f871
novahosting1 <staffcetrios@gmail.com>
2026-03-10 13:41:58 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function login(){
+document.getElementById("login").classList.add("hidden")
+document.getElementById("panel").classList.remove("hidden")
+}
+
+function showRegister(){
+document.getElementById("login").classList.add("hidden")
+document.getElementById("register").classList.remove("hidden")
+}
+
+function showLogin(){
+document.getElementById("register").classList.add("hidden")
+document.getElementById("login").classList.remove("hidden")
+}
